bdc9240b351acce34f998420ca9d3a19b03e5ea2
naveenmathramkott <naveenmathramkott@gmail.com>
2024-09-24 12:23:21 +0530

    added meta data for open graph and twitter, facebook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,6 @@ import Header from "@/components/Header";
 import ViewCanvas from "@/components/ViewCanvas";
 import Footer from "@/components/Footer";
 
-export const metadata = {
-  title: "Cocotein - electrolyte fruit water",
-  description: "Discover the refreshing taste of Cocotein",
-};
-
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
   display: "swap",
@@ -27,6 +22,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={alpino.variable}>
+      <head>
+        {/* Primary Meta Tags */}
+        <title>Cocotein - electrolyte fruit water</title>
+        <meta name="title" content="Cocotein - electrolyte fruit water" />
+        <meta
+          name="description"
+          content="Discover the refreshing taste of Cocotein."
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cocotein.vercel.app/" />
+        <meta
+          property="og:title"
+          content="Cocotein - electrolyte fruit water"
+        />
+        <meta
+          property="og:description"
+          content="Discover the refreshing taste of Cocotein"
+        />
+        <meta property="og:image" content="./screenshot.png" />
+
+        {/* Twitter */}
+        <meta
+          property="twitter:card"
+          content="Cocotein - electrolyte fruit water"
+        />
+        <meta property="twitter:url" content="https://cocotein.vercel.app/" />
+        <meta property="twitter:title" content="Homepage" />
+        <meta
+          property="twitter:description"
+          content="Discover the refreshing taste of Cocotein."
+        />
+        <meta property="twitter:image" content="./screenshot.png" />
+      </head>
       <body className="overflow-x-hidden bg-yellow-300">
         <Header />
         <main>
